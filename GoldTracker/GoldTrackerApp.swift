2d4e599b53d663930586ec7eb92ff9086906d64c
vincent <vincent_gemini@sina.com>
2026-02-07 10:04:14 +0800

    Initial Commit

---
+++ GoldTracker/GoldTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoldTrackerApp.swift
+//  GoldTracker
+//
+//  Created by zhongyafeng on 2026/2/7.
+//
+
+import SwiftUI
+
+@main
+struct GoldTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
